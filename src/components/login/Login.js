c035c49b36403df33521907dacc3f5261466bf61
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-09 07:53:54 +0100

    added sidebar

---
+++ src/components/login/Login.js
@@ -0,0 +1,23 @@
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+import Logo from "../../assets/images/logo.png";
+import Style from "./Style";
+
+ const Login = () => {
+        const classes = Style();
+
+        const uiConfig = {
+            signInFlow: "popup",
+            signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+        };
+       
+        return (
+            <div className={classes.login}>
+                <img src={Logo} alt="linked-in-logo" className={classes.logo} />
+                <h4>Linked-In Clone</h4>
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+            </div>
+        );
+    };
+
+    export default Login;

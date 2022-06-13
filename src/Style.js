a88b760e4283aaa7690776c1df383f0039b4ef57
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-13 12:28:20 +0100

    added loading animation

--- src/Style.js
+++ src/Style.js
@@ -5,28 +5,27 @@ export default makeStyles((theme) => ({
         position: "sticky",
         top: 0,
         zIndex: 100,
-        height: "7vh",
+        height: 50,
         display: "flex",
         justifyContent: "center",
     },
+
     app__body: {
-        minHeight: "93vh",
         display: "flex",
+        minHeight: "calc(100vh - 50px)",
         justifyContent: "center",
         paddingTop: 50,
         paddingBottom: 50,
         [theme.breakpoints.down("xs")]: {
             paddingTop: 10,
-            paddingBottom: 40,
+            paddingBottom: 55,
         },
     },
-
     body__sidebar: {
         minWidth: 150,
         maxWidth: 220,
         height: "auto",
     },
-
     body__feed: {
         minWidth: 500,
         display: "flex",
@@ -39,18 +38,14 @@ export default makeStyles((theme) => ({
             padding: 0,
         },
     },
-
     feed__form: {
         width: "100%",
         height: "auto",
-
     },
-
     feed__posts: {
         width: "100%",
         height: "auto",
     },
-
     body__widgets: {
         minWidth: 225,
         maxWidth: 300,
